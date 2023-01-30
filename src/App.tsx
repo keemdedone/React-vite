b3040a3490ee8132ee85f62cce655451214bb156
keemdedone <komchan447@gmail.com>
2023-01-30 16:42:37 +0700

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import { useState } from "react";
-import { Button } from "@mui/material";
-import reactLogo from "./assets/react.svg";
 import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function alertMsg(msg: string) {
-    alert('This is alert say " ' + msg + ' "');
-  }
-
   return <div className="App"></div>;
 }
 
