5445f46b4a4662f293a63414c7908d1f5560529d
keemdedone <komchan447@gmail.com>
2023-02-04 16:00:14 +0700

    weather complete

--- src/model/model.tsx
+++ src/model/model.tsx
@@ -91,5 +91,52 @@ export const monthName = [
 
 export const keys = {
   API_KEY: "d9e3f33539b4582e668b5097b2428c17",
-  BASE_URL: "https://api.openweathermap.org/data/3.0/",
+  BASE_URL: "https://api.openweathermap.org/data/2.5/",
+};
+
+export type WeatherAPI = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
 };
--- src/pages/weather/weather.tsx
+++ src/pages/weather/weather.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { keys } from "../../model/model";
+import { keys, WeatherAPI } from "../../model/model";
+import "./weather.scss";
 
 const api = {
   key: keys.API_KEY,
@@ -7,27 +8,69 @@ const api = {
 };
 
 const Weather = () => {
-  const dateSelect = (date: string) => {
-    const d = new Date().toString().slice(3, 15);
-    return d;
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState<WeatherAPI>();
+
+  const search = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+        .then((res) => res.json())
+        .then((results) => {
+          setQuery("");
+          setWeather(results);
+          console.log(results);
+        });
+    }
+  };
+
+  const dateSelect = (date: Date) => {
+    return date.toString().slice(3, 15);
   };
 
   return (
-    <div className="page-container">
-      <div className="search-box">
-        <input
-          type="text"
-          placeholder="location...."
-          className="search-input"
-        />
-      </div>
-      <div className="location-box">
-        <div className="location"></div>
-        <div className="date"></div>
-      </div>
-      <div className="weather-box">
-        <div className="temperature"></div>
-        <div className="weather-status"></div>
+    <div className="weather page-container">
+      <div
+        className={
+          new Date().getHours() <= 12
+            ? "weather-container morning"
+            : new Date().getHours() <= 18
+            ? "weather-container noon"
+            : new Date().getHours() > 18
+            ? "weather-container night"
+            : "weather-container"
+        }
+      >
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="location...."
+            className="search-input"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyDown={search}
+          />
+        </div>
+        {weather ? (
+          <div className="weather-description">
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateSelect(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temperature">
+                {weather.main.temp.toFixed(0)}
+                <span className="celsius-symbol">°C</span>
+              </div>
+              <div className="weather-status">
+                {weather.weather[0].main}, {weather.weather[0].description}
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
